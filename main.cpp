725c32e78ad75cb4d40705275ad88670d2b3db46
Syed Muhammad Shoaib Shah <231283405+ScarFYTE@users.noreply.github.com>
2026-05-10 21:25:52 +0500

    Update main.cpp for SFML 3.x API

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@
 #include <iostream>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(900, 600), "Mutual Fund Investment Tracker");
+    sf::RenderWindow window(sf::VideoMode({900u, 600u}), "Mutual Fund Investment Tracker");
     window.setFramerateLimit(60);
 
     sf::Font font;
-    if (!font.loadFromFile("arial.ttf")) {
+    if (!font.openFromFile("arial.ttf")) {
         std::cout << "Font file missing (arial.ttf). Place it in the project folder.\n";
         return 0;
     }
@@ -24,31 +24,46 @@ int main() {
 
     fileManager.loadData(user, portfolio);
 
-    sf::Text title("Mutual Fund Investment Tracker", font, 24);
-    title.setPosition(20, 20);
+    sf::Text title;
+    title.setFont(font);
+    title.setString("Mutual Fund Investment Tracker");
+    title.setCharacterSize(24);
+    title.setPosition({20.f, 20.f});
 
-    sf::Text info("", font, 16);
-    info.setPosition(20, 70);
+    sf::Text info;
+    info.setFont(font);
+    info.setCharacterSize(16);
+    info.setPosition({20.f, 70.f});
 
-    sf::Text fundsText("", font, 16);
-    fundsText.setPosition(20, 130);
+    sf::Text fundsText;
+    fundsText.setFont(font);
+    fundsText.setCharacterSize(16);
+    fundsText.setPosition({20.f, 130.f});
 
-    sf::Text portfolioText("", font, 16);
-    portfolioText.setPosition(20, 340);
+    sf::Text portfolioText;
+    portfolioText.setFont(font);
+    portfolioText.setCharacterSize(16);
+    portfolioText.setPosition({20.f, 340.f});
 
-    sf::RectangleShape buyBtn(sf::Vector2f(120, 35));
+    sf::RectangleShape buyBtn(sf::Vector2f(120.f, 35.f));
     buyBtn.setFillColor(sf::Color(100, 200, 120));
-    buyBtn.setPosition(700, 150);
+    buyBtn.setPosition({700.f, 150.f});
 
-    sf::RectangleShape sellBtn(sf::Vector2f(120, 35));
+    sf::RectangleShape sellBtn(sf::Vector2f(120.f, 35.f));
     sellBtn.setFillColor(sf::Color(200, 100, 100));
-    sellBtn.setPosition(700, 200);
+    sellBtn.setPosition({700.f, 200.f});
 
-    sf::Text buyText("Buy 10 Units", font, 14);
-    buyText.setPosition(710, 157);
+    sf::Text buyText;
+    buyText.setFont(font);
+    buyText.setString("Buy 10 Units");
+    buyText.setCharacterSize(14);
+    buyText.setPosition({710.f, 157.f});
 
-    sf::Text sellText("Sell 10 Units", font, 14);
-    sellText.setPosition(710, 207);
+    sf::Text sellText;
+    sellText.setFont(font);
+    sellText.setString("Sell 10 Units");
+    sellText.setCharacterSize(14);
+    sellText.setPosition({710.f, 207.f});
 
     while (window.isOpen()) {
         sf::Event event;
@@ -59,7 +74,8 @@ int main() {
             }
 
             if (event.type == sf::Event::MouseButtonPressed) {
-                sf::Vector2f mouse(event.mouseButton.x, event.mouseButton.y);
+                sf::Vector2f mouse(static_cast<float>(event.mouseButton.x),
+                                  static_cast<float>(event.mouseButton.y));
 
                 if (buyBtn.getGlobalBounds().contains(mouse)) {
                     portfolio.buyUnits(user, market.getFunds()[0], 10);
